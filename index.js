beb31dd3f85836c1a2c917870a622d9a9f22f371
harshcut <66206863+harshcut@users.noreply.github.com>
2020-12-18 15:06:11 +0530

    feat: init express

---
+++ index.js
@@ -0,0 +1,15 @@
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("https://github.com/harshcut/face-model-api");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Express server listening on port ${process.env.PORT || 3000}`);
+});
