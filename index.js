6ba14bb0a84747f8d024bdfbf74ae7ee677540bd
harshcut <66206863+harshcut@users.noreply.github.com>
2020-12-19 15:14:04 +0530

    feat: init pg database

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const cors = require("cors");
 const express = require("express");
+const knex = require("knex");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 app.use(express.json());
+
+const database = knex({
+  client: "pg",
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'post',
+    database: 'face-model-storage',
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("https://github.com/harshcut/face-model-api");
